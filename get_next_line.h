f0603fe0d1ee9fc4a26dcb444cd53652d2c37d10
JonathanLimbada <50628608+JonathanLimbada@users.noreply.github.com>
2019-06-13 09:34:39 +0200

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_next_line.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlimbada <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 17:06:14 by jlimbada          #+#    #+#             */
+/*   Updated: 2019/06/13 09:20:29 by jlimbada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+
+# include "./libft/libft.h"
+
+int				get_next_line(const int fd, char **line);
+
+#endif
